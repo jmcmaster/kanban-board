ca5158578411119132c89276fa3bd45e0cca3d2d
Jason McMaster <jmcmaster@pushhere.com>
2018-06-16 16:45:49 -0400

    Adds css styling to the board page.

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,20 +3,28 @@ import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import CardList from '../components/CardList';
 import CreateListContainer from '../containers/CreateListContainer';
+import '../styles/styles.css';
 
 class Board extends Component {
   renderBoard = () => {
     const { board } = this.props;
     if (board.lists.length) {
-      return board.lists.map((list) => (
-        <div key={list.id}>
+      return (
+        <div className="Board">
+        {board.lists.map((list) => (
+
           <CardList
+            key={list.id}
             list={list}
             board={board}
             cards={board.cards.filter((card) => card.list === list.id)}
           />
+
+
+        ))}
         </div>
-      ));
+      );
+
     }
     else {
       return <h3>No lists yet</h3>
